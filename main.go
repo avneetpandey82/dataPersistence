0de127ce628fc3e1427caf6068bc9f1d2e451f9e
avneetpandey82 <avneetpandey82@gmail.com>
2024-11-22 14:07:29 -0500

    cors enabled & basic IP handler function is created

--- main.go
+++ main.go
@@ -5,8 +5,22 @@ import (
 	"net/http"
 )
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
+func saveIPHandler(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	if r.Method != "POST" {
+		http.Error(w, "Only POST requests are allowed", http.StatusBadRequest)
+	}
+
+}
 func main() {
 	fs := http.FileServer(http.Dir("./templates"))
 	http.Handle("/", fs)
+	http.HandleFunc("/api/saveIP", saveIPHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
